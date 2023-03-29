005d6e95de9d4725b99d49b53d57bfaff51563a4
Kuromika <kuromika.dev@gmail.com>
2023-03-29 12:53:54 -0500

    Add post controller for sign up

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,31 @@
 const { body, validationResult } = require("express-validator");
 const Message = require("../models/message");
+const User = require("../models/user");
+const { generatePassword } = require("../lib/password");
 
 const getSignUp = (req, res, next) => {
   res.render("signup");
+};
+
+const postSignUp = async (req, res, next) => {
+  const { salt, genHash } = generatePassword(req.body.password);
+  const userCount = await User.countDocuments({});
+  const newUser = new User({
+    username: req.body.username,
+    hash: genHash,
+    salt,
+    fakerUsername: `Weeb Member ${userCount.toString().padStart(3, "0")}`,
+  });
+  try {
+    await newUser.save();
+  } catch (err) {
+    return next(err);
+  }
+  return res.redirect("/");
 };
 
 const getLogIn = (req, res, next) => {
   res.render("login");
 };
 
-module.exports = { getLogIn, getSignUp };
+module.exports = { getLogIn, getSignUp, postSignUp };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getSignUp, getLogIn } = require("../controllers/index");
+const { getSignUp, getLogIn, postSignUp } = require("../controllers/index");
 
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
 router.get("/signup", getSignUp);
+router.post("/signup", postSignUp);
+
 router.get("/login", getLogIn);
 
 module.exports = router;
