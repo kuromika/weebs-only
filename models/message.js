d9af2d16113f0abc49d0b5ac40813c20b6c9388d
Kuromika <kuromika.dev@gmail.com>
2023-03-29 16:59:34 -0500

    Add virtual for short date in message

--- models/message.js
+++ models/message.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const MessageSchema = new mongoose.Schema({
   text: { type: String, required: true },
   date: { type: Date, default: Date.now },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+MessageSchema.virtual("dateFormatted").get(function getDate() {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_SHORT);
 });
 
 const Message = mongoose.model("Message", MessageSchema);
