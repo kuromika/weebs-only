2b4ae9642e15adbe98704262250d068ac3679d4f
Kuromika <kuromika.dev@gmail.com>
2023-03-29 11:18:54 -0500

    Add fake username field

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
+  fakerUsername: { type: String, required: true },
   hash: { type: String, required: true },
   salt: { type: String, required: true },
   isMember: { type: Boolean, default: false },
