01374c179a34cc1daed108ba0b245d22a48ca6e4
Kuromika <kuromika.dev@gmail.com>
2023-03-30 13:03:53 -0500

    Add profile picture to user model

--- models/user.js
+++ models/user.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   fakeUsername: { type: String, required: true },
+  profilePicture: {
+    type: String,
+    default: "/images/defaultpfp.png",
+    required: true,
+  },
   hash: { type: String, required: true },
   salt: { type: String, required: true },
   isMember: { type: Boolean, default: false },
